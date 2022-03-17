b9771e3dbd416822aecb2cded967870f3459ef89
Ravella Rodrigues <thuanyravella@gmail.com>
2022-03-17 01:03:31 -0400

    trabalho-gerencia-de-config

---
+++ tests/test_multiplication.py
@@ -0,0 +1,11 @@
+from src.python_calculator import multiply 
+import pytest
+
+def test_multiply():
+    result = multiply(3, 4)
+    assert result == 12
+
+
+def test_multiply_string():
+    with pytest.raises(TypeError):
+        multiply("string", 4)
---
+++ tests/test_subtraction.py
@@ -0,0 +1,18 @@
+import pytest
+from src.python_calculator import subtract
+
+
+
+def test_subtract_positive():
+    result = subtract(4, 3)
+    assert result == 1
+
+
+def test_subtract_negative():
+    result = subtract(3, 4)
+    assert result == -1
+
+
+def test_subtract_string():
+    with pytest.raises(TypeError):
+        subtract("string", 4)
